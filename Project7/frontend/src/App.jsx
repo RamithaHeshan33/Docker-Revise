c0a184da7fb99566229cbf5503579fa5ace5935a
Ramitha Heshan <ramithacampus@gmail.com>
2025-04-22 12:54:21 +0530

    frontend dockerization

---
+++ Project7/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to the React App</h1>
+      <p>This is a simple React application.</p>
+      <p>It is designed to demonstrate how to work with React and Docker.</p>
+      <p>This line checks the dynamically update</p>
+      <p>This line checks the dynamically update 69</p>
+
+    </>
+  )
+}
+
+export default App
